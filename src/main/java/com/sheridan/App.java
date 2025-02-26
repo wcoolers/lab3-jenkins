32f196c8a2b2cd10c0ee87839b608e2f973941f1
Adegoke Akanbi <16087140+wcoolers@users.noreply.github.com>
2025-02-25 22:11:51 -0500

    initial commit, added additional print statement

---
+++ src/main/java/com/sheridan/App.java
@@ -0,0 +1,15 @@
+package com.sheridan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String name = "Adegoke Akanbi";
+        System.out.println("My name is " + name);
+        System.out.println( "Hello World!" );
+    }
+}
